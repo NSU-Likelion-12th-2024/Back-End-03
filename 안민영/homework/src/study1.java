cda38443e263aa97932683ba1c92e78dd8feafdd
ANMNYG <dkslasdud@gmail.com>
2024-03-31 19:03:03 +0900

    JVM과자바구동원리

---
+++ 안민영/homework/src/study1.java
@@ -0,0 +1,11 @@
+public class study1 {
+    public static void main(String [] args) {
+        for (int i = 0 ; i <= 6 ; i++) {
+            for (int j = 0 ; j < i * 2 ; j++) {
+
+            }
+            System.out.println("*");
+
+        }
+    }
+}
